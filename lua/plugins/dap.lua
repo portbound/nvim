2bf0ad9cd39ba0e0d3b39995b900704754ae898e
portbound <prtbnd@proton.me>
2025-07-23 10:03:44 -0400

    Removed Mason integration with debugger

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,31 +1,14 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-	-- NOTE: Yes, you can install new plugins here!
 	"mfussenegger/nvim-dap",
-	-- NOTE: And you can specify dependencies as well
 	dependencies = {
-		-- Creates a beautiful debugger UI
 		"rcarriga/nvim-dap-ui",
-
-		-- Required dependency for nvim-dap-ui
 		"nvim-neotest/nvim-nio",
-
-		-- Installs the debug adapters for you
-		"williamboman/mason.nvim",
-		"jay-babu/mason-nvim-dap.nvim",
-
-		-- Add your own debuggers here
 		"leoluz/nvim-dap-go",
+
+		-- Shows variable balues inline as virtual text
+		-- "theHamsta/nvim-dap-virtual-text",
 	},
 	keys = {
-		-- Basic debugging keymaps, feel free to change to your liking!
 		{
 			"<F5>",
 			function()
@@ -84,33 +67,11 @@ return {
 			desc = "Debug: See last session result.",
 		},
 	},
+
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
-
-		require("mason-nvim-dap").setup({
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
-			automatic_installation = false,
-
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
-			handlers = {},
-
-			-- You'll need to check that you have the required things installed
-			-- online, please don't ask me how to install them :)
-			ensure_installed = {
-				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
-			},
-		})
-
-		-- Dap UI setup
-		-- For more information, see |:help nvim-dap-ui|
 		dapui.setup({
-			-- Set icons to characters that are more likely to work in every terminal.
-			--    Feel free to remove or use ones that you like more! :)
-			--    Don't feel like these are good choices.
 			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
 			controls = {
 				icons = {
@@ -127,24 +88,23 @@ return {
 			},
 		})
 
-		-- Change breakpoint icons
 		vim.api.nvim_set_hl(0, "DapBreak", { fg = "#e51400" })
 		vim.api.nvim_set_hl(0, "DapStop", { fg = "#ffcc00" })
 		local breakpoint_icons = vim.g.have_nerd_font
-		    and {
-			    Breakpoint = "",
-			    BreakpointCondition = "",
-			    BreakpointRejected = "",
-			    LogPoint = "",
-			    Stopped = "",
-		    }
-		    or {
-			    Breakpoint = "●",
-			    BreakpointCondition = "⊜",
-			    BreakpointRejected = "⊘",
-			    LogPoint = "◆",
-			    Stopped = "⭔",
-		    }
+			and {
+				Breakpoint = "",
+				BreakpointCondition = "",
+				BreakpointRejected = "",
+				LogPoint = "",
+				Stopped = "",
+			}
+			or {
+				Breakpoint = "●",
+				BreakpointCondition = "⊜",
+				BreakpointRejected = "⊘",
+				LogPoint = "◆",
+				Stopped = "⭔",
+			}
 		for type, icon in pairs(breakpoint_icons) do
 			local tp = "Dap" .. type
 			local hl = (type == "Stopped") and "DapStop" or "DapBreak"
@@ -155,9 +115,13 @@ return {
 		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 		dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
+
+		-- require("nvim-dap-virtual-text").setup()
+
 		-- Install golang specific config
 		require("dap-go").setup({
 			delve = {
+				path = vim.fn.exepath("dlv") ~= "" and vim.fn.exepath("dlv") or "/home/linuxbrew/.linuxbrew/bin/dlv",
 				-- On Windows delve must be run attached or it crashes.
 				-- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
 				detached = vim.fn.has("win32") == 0,
@@ -201,7 +165,7 @@ return {
 				name = "Delve: Debug All Tests (Module)",
 				request = "launch",
 				mode = "test",
-				program = "${workspaceFolder}", -- 🧠 must be root of the module (where go.mod lives)
+				program = "${workspaceFolder}", -- must be root of the module (where go.mod lives)
 			},
 			{
 				type = "go",
