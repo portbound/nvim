81ecaa3797ee08526ec405d918846acf8d45abbc
portbound <prtbnd@proton.me>
2025-07-24 16:24:26 -0400

    added borders to LSP hover

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,75 @@
+return {
+    { "L3MON4D3/LuaSnip", keys = {} },
+    {
+        "saghen/blink.cmp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+        },
+        version = "*",
+        config = function()
+            require("blink.cmp").setup({
+                snippets = {
+                    expand = function(snippet) vim.snippet.expand(snippet) end,
+                    active = function(filter)
+                        return vim.snippet.active(filter)
+                    end,
+                    jump = function(direction)
+                        vim.snippet.jump(direction)
+                    end,
+                },
+                signature = { enabled = true },
+                appearance = {
+                    use_nvim_cmp_as_default = false,
+                    nerd_font_variant = "normal",
+                },
+                sources = {
+                    default = { "lsp", "path", "snippets", "buffer" },
+                    providers = {
+                        cmdline = {
+                            min_keyword_length = 2,
+                        },
+                    },
+                },
+                keymap = {
+                    ["<CR>"] = { "accept", "fallback" },
+                    ['<Tab>'] = { 'snippet_forward', 'fallback' },
+                    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+                    ["<C-f>"] = {},
+                },
+                cmdline = {
+                    enabled = false,
+                    completion = { menu = { auto_show = true } },
+                    keymap = {
+                        ["<CR>"] = { "accept_and_enter", "fallback" },
+                    },
+                },
+                completion = {
+                    menu = {
+                        border = "rounded",
+                        scrolloff = 1,
+                        scrollbar = false,
+                        draw = {
+                            columns = {
+                                { "kind_icon" },
+                                { "label",      "label_description", gap = 1 },
+                                { "kind" },
+                                { "source_name" },
+                            },
+                        },
+                    },
+                    documentation = {
+                        window = {
+                            border = "rounded",
+                            scrollbar = false,
+                            winhighlight =
+                            "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+                        },
+                        auto_show = true,
+                        auto_show_delay_ms = 500,
+                    },
+                },
+            })
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,4 +1,3 @@
-
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
@@ -15,7 +14,6 @@ return {
 			background_colour = "#000000",
 			stages = "slide",
 		})
-
 		require("noice").setup({
 			lsp = {
 				override = {
@@ -23,14 +21,61 @@ return {
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true,
 				},
+				-- hover = {
+				-- 	enabled = false,
+				-- },
+				signature = {
+					enabled = true,
+					auto_open = {
+						enabled = true,
+						trigger = true,
+						luasnip = true,
+						throttle = 50,
+					},
+				},
 			},
 			presets = {
 				bottom_search = true,
 				command_palette = true,
 				long_message_to_split = true,
 				inc_rename = false,
-				lsp_doc_border = false,
+				lsp_doc_border = true, -- Can set back to false since we're not using noice hover
 			},
 		})
 	end,
 }
+-- return {
+-- 	"folke/noice.nvim",
+-- 	event = "VeryLazy",
+-- 	opts = {
+-- 		-- add any options here
+-- 	},
+-- 	dependencies = {
+-- 		"MunifTanjim/nui.nvim",
+-- 		"rcarriga/nvim-notify",
+-- 	},
+-- 	config = function()
+-- 		-- Configure notify first
+-- 		require("notify").setup({
+-- 			background_colour = "#000000",
+-- 			stages = "slide",
+-- 		})
+--
+-- 		require("noice").setup({
+-- 			lsp = {
+-- 				override = {
+-- 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+-- 					["vim.lsp.util.stylize_markdown"] = true,
+-- 					["cmp.entry.get_documentation"] = true,
+-- 				},
+-- 			},
+-- 			presets = {
+-- 				bottom_search = true,
+-- 				command_palette = true,
+-- 				long_message_to_split = true,
+-- 				inc_rename = false,
+-- 				lsp_doc_border = true,
+-- 			},
+-- 		})
+-- 	end,
+-- }
