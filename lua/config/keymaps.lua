2108c5304108d554127c9afd196407ef1c251115
portbound <token.api.guy@gmail.com>
2025-08-01 09:11:17 -0400

    added keymap for error history

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,5 +8,5 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>e", ":Ex<cr>", { desc = "Open [E]xplorer" })
+vim.keymap.set("n", "<leader>eh", ":Noice history<CR>", { desc = "[E]rror [H]istory" })
 vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
