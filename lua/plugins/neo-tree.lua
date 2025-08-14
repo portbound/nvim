cdb04fa84629cc88eeb1663b6f3a49ba752ce438
portbound <token.api.guy@gmail.com>
2025-08-14 01:56:54 -0400

    removed lazy=false from neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,6 @@ return {
 		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
 		'MunifTanjim/nui.nvim',
 	},
-	lazy = false,
 	keys = {
 		{ '<leader>-', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
 	},
