3b64fb1429c56ec761263edc2bcfb318b8330316
portbound <token.api.guy@gmail.com>
2025-08-14 01:51:06 -0400

    add neotree remove yazi

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+return {
+	'nvim-neo-tree/neo-tree.nvim',
+	version = '*',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+	},
+	lazy = false,
+	keys = {
+		{ '<leader>-', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+	},
+	opts = {
+		event_handlers = {
+			{
+				event = "neo_tree_buffer_enter",
+				handler = function()
+					vim.opt_local.relativenumber = true
+				end,
+			}
+		},
+		filesystem = {
+			window = {
+				mappings = {
+					['<leader>-'] = 'close_window',
+				},
+			},
+		},
+	},
+}
