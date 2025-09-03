794883a28e1fd6671c1e6f437aa2a6c492bfef65
portbound <token.api.guy@gmail.com>
2025-09-03 12:14:54 -0400

    updated plugin: noice - centered cmd bar in viewport

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -37,7 +37,7 @@ return {
 			},
 			presets = {
 				bottom_search = true,
-				command_palette = true,
+				command_palette = false,
 				long_message_to_split = true,
 				inc_rename = false,
 				lsp_doc_border = true, -- Can set back to false since we're not using noice hover
@@ -45,38 +45,3 @@ return {
 		})
 	end,
 }
--- return {
--- 	"folke/noice.nvim",
--- 	event = "VeryLazy",
--- 	opts = {
--- 		-- add any options here
--- 	},
--- 	dependencies = {
--- 		"MunifTanjim/nui.nvim",
--- 		"rcarriga/nvim-notify",
--- 	},
--- 	config = function()
--- 		-- Configure notify first
--- 		require("notify").setup({
--- 			background_colour = "#000000",
--- 			stages = "slide",
--- 		})
---
--- 		require("noice").setup({
--- 			lsp = {
--- 				override = {
--- 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
--- 					["vim.lsp.util.stylize_markdown"] = true,
--- 					["cmp.entry.get_documentation"] = true,
--- 				},
--- 			},
--- 			presets = {
--- 				bottom_search = true,
--- 				command_palette = true,
--- 				long_message_to_split = true,
--- 				inc_rename = false,
--- 				lsp_doc_border = true,
--- 			},
--- 		})
--- 	end,
--- }
