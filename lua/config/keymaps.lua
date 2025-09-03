ea8567474b9e62055b8438d6cb69f80635a02306
portbound <token.api.guy@gmail.com>
2025-09-03 12:17:41 -0400

    update keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,15 +1,11 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
--- Commenting quick fix in favor of trouble nvim
--- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("n", "L", "$", { desc = "Jump to EOL"})
-vim.keymap.set("n", "H", "^", { desc = "Jump to SOL"})
+vim.keymap.set("n", "L", "$", { desc = "Jump to EOL" })
+vim.keymap.set("n", "H", "^", { desc = "Jump to SOL" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
-vim.keymap.set("n", "<leader>eh", ":Noice history<CR>", { desc = "[E]rror [H]istory" })
-vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
