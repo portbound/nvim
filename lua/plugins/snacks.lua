52e4af2170fe160af7a98f42692e1fd080a8f3bc
portbound <token.api.guy@gmail.com>
2025-09-08 14:48:57 -0400

    disabling 99% of snacks - just want the dash

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
-		bigfile = { enabled = true },
+		-- bigfile = { enabled = true },
 		dashboard = {
 			preset = {
 				header = [=[
@@ -52,13 +52,13 @@ return {
 		},
 		--explorer = { enabled = true },
 		--indent = { enabled = true },
-		input = { enabled = true },
+		-- input = { enabled = true },
 		--picker = { enabled = true },
 		--notifier = { enabled = true },
-		quickfile = { enabled = true },
-		scope = { enabled = true },
-		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
+		-- quickfile = { enabled = true },
+		-- scope = { enabled = true },
+		-- scroll = { enabled = true },
+		-- statuscolumn = { enabled = true },
+		-- words = { enabled = true },
 	},
 }
