6bdef66f5bef5df8988e5cb0ba3170de07798ed0
portbound <token.api.guy@gmail.com>
2025-10-22 12:26:04 -0400

    added theme kanso

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,19 @@
+-- return {
+-- 	"webhooked/kanso.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("kanso").setup({
+-- 			transparent = true,
+-- 			functionStyle = {
+-- 				bold = true
+-- 			},
+-- 		})
+-- 		vim.cmd.colorscheme("kanso")
+-- 	end
+-- }
+
 return {
-	-- "webhooked/kanso.nvim",
-	-- priority = 1000,
-	-- config = function()
-	-- 	require('kanso').setup({
-	-- 		transparent = true,
-	-- 		colors = {
-	-- 			palette = {
-	-- 				def = "#8ba4b0",
-	-- 				-- keyword = "#8989A7",
-	-- 				fn = "#8989A7",
-	-- 				-- fn = "#A78989",
-	-- 			}
-	-- 		},
-	-- 		overrides = function(colors)
-	-- 			return {
-	-- 				-- ['@keyword'] = { fg = colors.palette.keyword },
-	-- 				['@function'] = { fg = colors.palette.fn },
-	-- 				['@function.call'] = { fg = colors.palette.def },
-	-- 				['@function.builtin'] = { fg = colors.palette.def },
-	-- 				['@function.method'] = { fg = colors.palette.fn },
-	-- 				['@function.method.call'] = { fg = colors.palette.def },
-	-- 				-- ['@type'] = { fg = colors.palette.tp }
-	-- 			}
-	-- 		end
-	-- 	})
-	-- 	vim.cmd.colorscheme("kanso")
-	-- end
 	"vague2k/vague.nvim",
 	priority = 1000,
 	config = function()
