b934093eb2414e3413d5bd2df74a4b2abefb3237
portbound <token.api.guy@gmail.com>
2025-10-26 15:31:46 -0400

    tweaking theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -20,11 +20,8 @@ return {
 		require("vague").setup({
 			comments = "italic",
 			transparent = true,
-			style = {
-				style = {},
-			},
 		})
 		vim.cmd.colorscheme("vague")
-		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#E0A363", bold = true })
+		-- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#E0A363", bold = true })
 	end,
 }
