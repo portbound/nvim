9af570d799b4f18416f74c642f14975e8ee0103b
portbound <token.api.guy@gmail.com>
2025-11-19 21:22:38 -0500

    open help in vertical split and disable autocomment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -30,6 +30,18 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	command = "wincmd L"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end
+})
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 	callback = function(event)
