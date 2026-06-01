808c4f96ecc868ef709fdc4b9b5bfa1bd870618f
portbound <token.api.guy@gmail.com>
2026-06-01 13:22:24 -0400

    removed marks nvim for mstache

--- plugin/marks.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({ "https://github.com/chentoast/marks.nvim" })
-require("marks").setup({
-	mappings = {
-		set_next = "mk",
-		delete_line = "dmk"
-	}
-})
---
+++ plugin/mstache.lua
@@ -0,0 +1,2 @@
+vim.pack.add({ "https://github.com/portbound/mstache.nvim" })
+require("mstache").setup()
