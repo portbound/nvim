45ba332968eafec5a6c02e94e7c700b423f63680
portbound <jake@portbound.io>
2026-06-22 21:21:23 -0400

    cleanup

--- init.lua
+++ init.lua
@@ -1,17 +1,11 @@
--- require("options")
--- require("keymaps")
--- require("autocmds")
--- require("diagnostics")
--- require("lua.theme.kanso")
-
 require("autocommands")
 require("diagnostics")
 require("options")
 require("keymaps")
 require("theme")
 
-vim.cmd("packadd nvim.undotree")
-vim.cmd("packadd nvim.difftool")
+-- vim.cmd("packadd nvim.undotree")
+-- vim.cmd("packadd nvim.difftool")
 
 vim.cmd('cnoreabbrev W w')
 vim.cmd('cnoreabbrev Q q')
