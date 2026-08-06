a7ea1f3484cd4e94c68caa1e06748f39c67d7e34
portbound <jake@portbound.io>
2026-08-05 22:00:37 -0400

    lualine theming

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -3,9 +3,27 @@ vim.pack.add({
 	"https://github.com/nvim-lualine/lualine.nvim",
 })
 
+local custom_theme = require('lualine.themes.auto')
+
+custom_theme.visual.a = {
+	bg = '#B4BE82',
+	fg = 'black',
+	gui = 'bold',
+}
+
+custom_theme.normal.b = {
+	bg = '#171717',
+	fg = '#a2b5c1'
+}
+custom_theme.normal.c = {
+	bg = '#202020',
+	fg = '#e1e1e1'
+}
+
 require("lualine").setup({
 	options = {
 		-- theme = "iceberg",
+		theme = custom_theme,
 		disabled_filetypes = { statusline = {}, winbar = {} },
 		section_separators = { "|", "|" }, -- Use a vertical bar
 		component_separators = { " ", " " }, -- Use spaces
